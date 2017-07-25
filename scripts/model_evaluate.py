d7d789c028d7dc652ec7172fc96170fbbf7bf219
Zhan <ZHANHA2@novartis.net>
2017-07-25 22:45:21 +0800

    update

--- scripts/model_evaluate.py
+++ scripts/model_evaluate.py
@@ -2,10 +2,12 @@
 #author ：zxst
 #time : 2017/07/24
 def R_square(x,y):
-    if ('numpy.ndarray' in str(type(x)))&('numpy.ndarray' in str(type(y)):
-        sse = sum((x-y)**2))
+    if ('numpy.ndarray' in str(type(x)))&('numpy.ndarray' in str(type(y))):
+        sse = sum((x-y)**2)
         sst = sum((y-np.mean(y))**2)
         R_square = 1-sse/sst
         return(R_square)
     else:
         print("please input array")
+
+##接下来是分类为题
